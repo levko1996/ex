de53eb946c80353e2ad5956ab2066b14de847c20
Misha <misha.levko96@gmail.com>
2021-04-24 09:55:30 +0300

    finish base logic

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,26 +1,48 @@
 import { ExelComponent } from '@core/ExelComponent'
+import { $ } from '@core/dom';
 
 export class Formula extends ExelComponent {
    static className = 'excel__formula'
 
-   constructor($root) {
+   constructor($root, options) {
       super($root, {
          name: 'Formula',
-         listeners: ['input', 'click']
+         listeners: ['input', 'keydown'],
+         ...options
       })
    }
 
    toHTML() {
       return `
          <div class="info">fx</div>
-         <div class="input" contenteditable spellcheck="false"></div>
+         <div
+            id="formula"
+            class="input"
+            contenteditable
+            spellcheck="false">
+         </div>
       `
    }
 
-   onInput(e) {
-      console.log(this.$root);
-      console.log('Formula: onInput', e);
+   init() {
+      super.init()
+      this.$formula = this.$root.find('#formula')
+      this.$on('table:select', $cell => {
+         this.$formula.text($cell.text())
+      })
+      this.$on('table:input', $cell => {
+         this.$formula.text($cell.text())
+      })
    }
-   onClick(e) {
+
+   onInput(e) {
+      this.$emit('formula:input', $(e.target).text())
+   }
+   onKeydown(e) {
+      const keys = ['Enter', 'Tab']
+      if (keys.includes(e.key)) {
+         e.preventDefault();
+         this.$emit('formula:done')
+      }
    }
 }
